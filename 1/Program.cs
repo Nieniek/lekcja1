3afa92e1e0e644b87164d46905e3dbf5e81755af
Marek Błaszczyk <mareczekblaszczyk@gmail.com>
2023-03-17 12:19:25 +0100

    Dodaj pliki projektów.

---
+++ 1/Program.cs
@@ -0,0 +1,11 @@
+﻿//poproś użytkownika o podanie imienia
+Console.WriteLine("Podaj swoje imię:");
+//deklaracja nową zmienną typu tekstowego o nazwie "name"
+string name = "";
+//odczytamy od użytkownika i zapiszemy od zmiennej imię - zapisz nieznajomy jeśli uzytkownik nie udzieli odpowiedzi
+name = Console.ReadLine() ?? "nieznajomy";
+//nowa zmienna - powitanie
+string greeting = "Witaj " + name + "!";
+//wpisz gotowe powitanie
+Console.WriteLine(greeting);
+
